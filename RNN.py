1ead7aa0dd6c4fc98a0803f389e4e38817e11b9c
JeanPJ <jeanpanaioti@gmail.com>
2016-03-31 10:33:09 -0300

    Inicialização da rede quase completa, há duvidas quanto aos fatores de escala dos reservatórios

--- RNN.py
+++ RNN.py
@@ -12,13 +12,14 @@ def Sparcity(M,psi):
                 N[linha][coluna] = 1
 
     return N*M
-class RNN
+class RNN:
 
-    def __init__(self,neu,n_in,n_out,gama,ro,psi,in_scale,bias_scale,alfa,forget = 1-10**-6.0,reg):
+    def __init__(self,neu,n_in,n_out,gama=0.5,ro=1,psi=0.5,in_scale=0.1,bias_scale=0.5,alfa=10,forget = 1-10**-6.0):
         self.Wrr = np.random.normal(0,1,[neu,neu]) #r = reservatorio, i = input, o = output, nomenclatura no modelo Wxy, sendo x a entrada e y a saida. b bias.
         self.Wir = np.random.normal(0,1,[neu,n_in])
         self.Wbr = np.random.normal(0,1,[neu,1])
         self.Wbo = np.random.normal(0,1,[n_out,1])
+        self.Wor = np.random.normal(0,1,[n_out,neu])
         self.leakrate = gama
         self.ro = ro
         self.psi = psi
@@ -26,14 +27,24 @@ class RNN
         self.bias_scale = bias_scale
         self.alfa = alfa
         self.forget = forget
-        self.reg = reg
         self.Wrr = Sparcity(self.Wrr,self.psi)
+        eigs = np.linalg.eigvals(self.Wrr)
+        print type(eigs)
+        radius = np.abs(np.max(eigs))
+        self.Wrr = self.Wrr/radius
+        self.Wrr = self.Wrr*ro
 
-    def Train:
-        #rotina de treinamento atraves dos minimos quadrados recursivos.
 
-    def update:
+    def Train(self):
+        pass
+        #rotina de treinamento atraves dos minimos quadrados recursivos atualiza Wor.
+
+    def Update(self):
+        pass
         #rotina de atualizacao dos estados, retorna a saida.
+
+
+RNN(100,1,1,0.9)
 
 
 
