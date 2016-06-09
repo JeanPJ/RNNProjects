fe0144876cd866f5c87118030ac74cac9e8fd88e
JeanPJ <jeanpanaioti@gmail.com>
2016-06-09 13:13:33 -0300

    Controlador faz o tanque com atraso seguir referencia

--- StirredTank.py
+++ StirredTank.py
@@ -7,6 +7,8 @@ from copy import *
 
 from RNN import *
 
+from scipy import signal
+
 def FindEquilibrium_q(T):
 
     Ti = 15.0 #temperatura de entrada
@@ -77,31 +79,53 @@ def UpdateTimeWindow(A,x): #Dado um array unidimensional A, elimina A[1] e acres
     B = np.append(B,x)
     return B
 
-def normalize(q):
-    qmin = 0.005
-    qmax = 0.03
-    x = (q-qmin)/(qmax - qmin)
+def normalize(q,mn,std):
+    x = (q-mn)/std
 
     return x
 
-def denormalize(x):
+def denormalize(x,mn,std):
 
-    qmin = 0.005
-    qmax = 0.03
-    q = (qmax - qmin)*x + qmin
+    q = std*x + mn
 
 
     return q
 
+def lowpass(x,u):
+    w0 = 5
+    f = u - w0*x
+    return f
+
+def RedNoise(x,t_step):
+
+    white_noise = np.random.randn()
+    xnext = RungeKutta4(t_step,lowpass,x,white_noise)
+    return xnext
+
+def RandomStair(t,stair_step,values): #vetor que gera uma sequencia aleatoria de valores, onde t eh um vetor de referencia, stair_step eh o numero de iteracoes que se mantem um valor
+                                      # e values sao os valores que o degrau pode assumir
+    values = np.array(values)
+    Stair = np.empty_like(t)
+    idx = 0
+    for i in range(Stair.size):
+        if i%stair_step == 0:
+            idx = np.random.randint(0,len(values))  # escolhe o indice aleatoriamente a cada passo da escada.
+        Stair[i] = values[idx]
+
+    return Stair
+
+
+
 #Parametros do problema.
-t_Stop = 700 #tempo corrente da simulacao
-t_step = 0.5 #periodo de amostragem da simulacao.
-t_Ctrl = 4 #Periodo de amostragem do controle.
+step_stop = 6000 #numero de passos totais, caso queira se usar essa metrica, no ponto de vista do controlador.
+t_step = 4 #periodo de amostragem da simulacao.
+
+First_step_end = 2000
 Vtube = 1.02 #volume no tubo de transporte
 
 T0 = 0 #condicao inicial
 
-t = np.arange(0,t_Stop,t_step) #tempo de simulacao
+t = np.arange(0,step_stop*t_step,t_step,dtype = np.float64) #tempo de simulacao
 
 iterations = range(len(t))
 Q_plot = np.empty_like(t)
@@ -121,20 +145,22 @@ qmax = 0.03
 
 qmin = 0.005
 
+qmn = 0.0250
+qstd = 0.0175
+
 Tmax = FindEquilibrium_T(qmin)
 
 Tmin = FindEquilibrium_T(qmax)
 
 # A Rede
 delta = 30
-delta_converted = int(round(delta*t_Ctrl/t_step))
 neurons = 500
 inputs = 2
 outputs = 1
-gama = 0.5
+gama = 0.49
 ro = 1
 psi = 0.5
-f_ir = 0.1
+f_ir = 0.08
 f_br = 0.5
 
 CtrlNetTrainer = rNN(neurons,inputs,outputs,gama,ro,psi,f_ir,f_br)
@@ -143,17 +169,42 @@ CtrlNetController = deepcopy(CtrlNetTrainer)
 
 #Criacao da janela da condicao inicial.
 
-q_past = np.empty_like(np.arange(delta_converted))
+q_past = np.empty_like(np.arange(delta))
 T_past = np.empty_like(q_past)
 Ttube_past =  np.empty_like(q_past)
 Tout_past = np.empty_like(q_past)
-Tfuturo = 40 #referencia degrau.
+Tfuturo = 0
 Tref_plot = np.empty_like(t)
 Training_Error_plot = np.empty_like(t)
 
 Control_plot = np.empty_like(t)
 
-for i in range(delta_converted):#Numero de passos necessarios para dar o equivalente a passagem no tempo de delta.
+#define vetor de referencias
+#for i in range(Tref_plot.size):
+#    if i < int(round(First_step_end/t_step)):
+#        Tfuturo = RedNoise(Tfuturo,t_step)
+#        Tref_plot[i] = 100*Tfuturo + 30
+#    else:
+#        break
+stair_step_ctrl = 200
+Tref_plot[:First_step_end] = 35 + 30*signal.square(2 * np.pi * 0.0005 * t[:First_step_end])
+Tref_plot[First_step_end:] = RandomStair(Tref_plot[First_step_end:],stair_step_ctrl,[30,40,50,60,55,45,40,35,30,32,33,49])
+
+#Tref_plot = RandomStair(Tref_plot,400,[30,40,50,60,55,45,40,35,30,32,33,49])
+
+Tmn = np.mean(Tref_plot)
+
+Tstd = np.std(Tref_plot)
+
+
+
+
+
+
+
+
+#uma rodada em malha aberta
+for i in range(delta):#Numero de passos necessarios para dar o equivalente a passagem no tempo de delta.
 
     T_past[i] = T
     Ttube_past[i] = Ttube
@@ -162,44 +213,41 @@ for i in range(delta_converted):#Numero de passos necessarios para dar o equival
     Ttube = RungeKutta4(t_step,Tube,Ttube,T)
     if i > np.floor(Vtube/(q*t_step)):
 
-        Tout = Ttube_past[i - np.floor(Vtube/(q*t_step))]
+        Tout = Ttube_past[i - int(round(Vtube/(q*t_step)))]
 
 
     Tout_past[i] = Tout + np.random.randn()
     q = 0.0167
 
-for i in iterations:
+for i in range(step_stop):
     #espaco para definicao do sinal de controle, onde entraria a rede
     #q = Q[i]
-    if i%(t_Ctrl/t_step) == 0:
     #etapa de treino
 
-        #Tmean = np.mean(Tout_past)
-       # Tstd = np.std(Tout_past)
+    #Tmean = np.mean(Tout_past)
+        #Tstd = np.std(Tout_past)
 
 
-        white_noise = np.random.randn()
-        CtrlNetTrainer.Update([(Tout_past[0]-Tmin)/(Tmax - Tmin),((Tout + white_noise)-Tmin)/(Tmax - Tmin)])
-        #CtrlNetTrainer.Update([(Tout_past[0]-Tmean)/Tstd,((Tout + white_noise)-Tmean)/Tstd])
-        u_ctrlpast = normalize(q_past[0])
-        CtrlNetTrainer.Train(u_ctrlpast)
-        trainingerror = CtrlNetTrainer.trainingError(u_ctrlpast)
+    white_noise = np.random.randn()
+    CtrlNetTrainer.Update([normalize(Tout_past[0],Tmn,Tstd),normalize(Tout,Tmn,Tstd)])
+    #CtrlNetTrainer.Update([(Tout_past[0]-Tmean)/Tstd,((Tout + white_noise)-Tmean)/Tstd])
+    u_ctrlpast = normalize(q_past[0],qmn,qstd)
+    CtrlNetTrainer.Train(u_ctrlpast)
+    trainingerror = CtrlNetTrainer.trainingError(u_ctrlpast)
 
 
     #etapa de teste
-        CtrlNetController.CopyWeights(CtrlNetTrainer)
-        uctrl = CtrlNetController.Update([((Tout + white_noise)-Tmin)/(Tmax-Tmin),(Tfuturo-Tmin)/(Tmax-Tmin)])
-        q = denormalize(uctrl)
-        #q = CtrlNetController.Update([((Tout + white_noise)-Tmean)/Tstd,(Tfuturo-Tmean)/Tstd])
+    CtrlNetController.CopyWeights(CtrlNetTrainer)
+    uctrl = CtrlNetController.Update([normalize(Tout,Tmn,Tstd),normalize(Tref_plot[i],Tmn,Tstd)])
+    #uctrl = CtrlNetController.Update([((Tout + white_noise)-Tmean)/Tstd,(Tfuturo-Tmean)/Tstd])
+    q = denormalize(uctrl,qmn,qstd)
+    control = q
 
-        control = q
+    if q > qmax:
+        q = qmax
 
-        if q > qmax:
-            q = qmax
-
-        if q < qmin:
-           q = qmin
-
+    if q < qmin:
+        q = qmin
 
 
     Training_Error_plot[i] = trainingerror
@@ -211,11 +259,11 @@ for i in iterations:
     Ttube = RungeKutta4(t_step,Tube,Ttube,T)
     if i > np.floor(Vtube/(q*t_step)):
 
-        Tout = Ttube_plot[i - round(Vtube/(q*t_step))]
+        Tout = Ttube_plot[i - int(round(Vtube/(q*t_step)))]
 
 
     Tout_plot[i] = Tout
-    Tref_plot[i] = Tfuturo
+    #Tref_plot[i] = Tfuturo
     Control_plot[i] = control
 
     q_past = UpdateTimeWindow(q_past,q)
@@ -225,27 +273,27 @@ for i in iterations:
 
 
 
-
+print Control_plot
 
 f, sub = plt.subplots(4, sharex=True)
 
 
 
-p1 = sub[0].plot(t,T_plot,label = 'Temperatura do tanque')
+#p1 = sub[0].plot(t,T_plot,label = 'Temperatura do tanque')
 
 
 
-p2 = sub[1].plot(t,Q_plot,label = 'Vazao Inlet')
+p2 = sub[1].plot(iterations,Q_plot,label = 'Vazao Inlet')
 
-p3 = sub[0].plot(t,Ttube_plot,label = 'Temperatura Tubo')
+#p3 = sub[0].plot(t,Ttube_plot,label = 'Temperatura Tubo')
 
-p4 = sub[0].plot(t,Tout_plot,label = 'Temperatura Saida')
+p4 = sub[0].plot(iterations,Tout_plot,label = 'Temperatura Saida')
 
-p5 = sub[0].plot(t,Tref_plot,label = 'Referencia')
+p5 = sub[0].plot(iterations,Tref_plot,label = 'Referencia')
 
-p6 = sub[2].plot(t,Training_Error_plot,label = 'erro de treinamento')
+p6 = sub[2].plot(iterations,Training_Error_plot,label = 'erro de treinamento')
 
-p7 = sub[3].plot(t,Control_plot,label = 'acao de controle')
+p7 = sub[3].plot(iterations,Control_plot,label = 'acao de controle')
 
 sub[0].legend()
 
@@ -255,17 +303,10 @@ sub[2].legend()
 
 sub[3].legend()
 
-plt.show(p1)
+#plt.show(p1)
 
 plt.show(p2)
 
-plt.show(p3)
+#plt.show(p3)
 
 plt.show(p4)
-
-
-
-
-
-
-
